2c0ce766cc0c1ac0dd054ba17e10d308fb599d6e
HelplessSoldier <patrick1234143211@gmail.com>
2023-08-23 16:05:24 -0700

    added createTodo input field

--- src/index.js
+++ src/index.js
@@ -2,16 +2,55 @@ import FolderContainer from "./modules/todoData/folderContainer";
 import Folder from "./modules/todoData/folder";
 import TodoItem from "./modules/todoData/todoItem";
 import DisplayTools from "./modules/display/display";
+import CreateTodo from "./modules/userInput/createTodo";
 
 const root = document.getElementById("content");
 const display = new DisplayTools(root);
 const folders = new FolderContainer();
 const defaultFolder = new Folder("Main");
 const item1 = new TodoItem("item1", "2077/24/23", 3);
+const createTodoDialogue = new CreateTodo(root);
+
+createTodoDialogue.createItemDialogue();
+
+item1.changePriority(2);
 
 folders.addFolder(defaultFolder);
-console.log(folders);
 defaultFolder.addItem(item1);
 
 display.folders(folders);
 display.items(defaultFolder);
+
+document.addEventListener("DOMContentLoaded", () => {
+    const createItemContainer = document.getElementById("createItemContainer");
+    const titleInput = document.getElementById("titleInput");
+    const dueDateInput = document.getElementById("dueDateInput");
+    const priorityInputHigh = document.getElementById("radioHigh");
+    const priorityInputMed = document.getElementById("radioMed");
+    const priorityInputLow = document.getElementById("radioLow");
+    const submitButton = document.getElementById("submitButton");
+    const closeButton = document.getElementById("closeButton");
+
+    submitButton.addEventListener("click", () => {
+        if (titleInput.value && dueDateInput.value) {
+            const dueDate = new Date(dueDateInput.value); // Convert input value to Date object
+            const currentDate = new Date();
+
+            if (dueDate > currentDate) {
+                let priority;
+                if (priorityInputHigh.checked) {
+                    priority = 3;
+                } else if (priorityInputMed.checked) {
+                    priority = 2;
+                } else if (priorityInputLow.checked) {
+                    priority = 1;
+                }
+            } else {
+                console.log("Due date must be in the future.");
+            }
+        }
+    });
+    closeButton.addEventListener("click", () => {
+        createItemContainer.style.display = "none";
+    });
+});
---
+++ src/modules/userInput/createTodo.js
@@ -0,0 +1,66 @@
+import createElement from "../helpers/createElement";
+
+class CreateTodo {
+    constructor(parent) {
+        this.parent = parent;
+    }
+    createItemDialogue() {
+        const container = createElement("div", { id: "createItemContainer" }, "");
+
+        // title input field and tag
+        const titleInputLabel = createElement(
+            "label",
+            { class: "InputLabel", for: "titleInput" },
+            "Title: "
+        );
+        const titleInput = createElement("input", { id: "titleInput", type: "text" });
+
+        // due date input field and tag
+        const dueDateInputLabel = createElement(
+            "label",
+            { class: "InputLabel", for: "dueDateInput" },
+            "DueDate: "
+        );
+        const dueDateInput = createElement("input", { id: "dueDateInput", type: "date" }, "");
+
+        // priority labels and radio buttons
+        const priorityInputLabel = createElement("label", { class: "InputLabel" }, "Priority: ");
+        const radioContainer = createElement("div", { class: "radioContainer" }, "");
+        const radioHigh = radioPair("High");
+        const radioMid = radioPair("Med");
+        const radioLow = radioPair("Low");
+        radioContainer.append(radioHigh, radioMid, radioLow);
+
+        // submitButton and closeButton
+        const buttonContainer = createElement("div", { class: "buttonContainer" }, "");
+        const submitButton = createElement("button", { id: "submitButton" }, "Submit");
+        const closeButton = createElement("button", { id: "closeButton" }, "Close");
+        buttonContainer.append(submitButton, closeButton);
+
+        container.append(
+            titleInputLabel,
+            titleInput,
+            dueDateInputLabel,
+            dueDateInput,
+            priorityInputLabel,
+            radioContainer,
+            buttonContainer
+        );
+
+        this.parent.append(container);
+    }
+}
+
+function radioPair(tag) {
+    const container = createElement("div", { class: "radioContainer" }, "");
+    const label = createElement("label", { for: `${tag}Radio`, class: "radioLabel" }, `${tag}`);
+    const radioButton = createElement(
+        "input",
+        { type: "radio", name: "priority", id: `radio${tag}` },
+        ""
+    );
+    container.append(label, radioButton);
+    return container;
+}
+
+export default CreateTodo;
