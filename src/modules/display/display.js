2fe85f7a8f3d6f6c029544c10304def9e3344be9
HelplessSoldier <patrick1234143211@gmail.com>
2023-08-25 14:13:06 -0700

    changed main folder to not be removable

--- src/modules/display/display.js
+++ src/modules/display/display.js
@@ -15,32 +15,39 @@ class DisplayTools {
                 dispatchEvent("folderClicked", folder);
             });
 
-            // remove button for each folder, X svg
-            const removeButton = createElement("div", { class: "removeButtonSvg" }, "");
-            const svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-            svgElement.setAttribute("width", "40px");
-            svgElement.setAttribute("height", "40px");
-            svgElement.setAttribute("viewBox", "0 0 24 24");
+            // remove button for each folder, except for main
+            // X svg hardcoded because i was having problems importing
+            // don't allow main to be deleted, was having errors when no folder existed
+            const removeButtonContainer = createElement("div", { id: "removeButtonContainer" }, "");
+            if (folder.name !== "Main") {
+                const removeButton = createElement("div", { class: "removeButtonSvg" }, "");
+                const svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+                svgElement.setAttribute("width", "40px");
+                svgElement.setAttribute("height", "40px");
+                svgElement.setAttribute("viewBox", "0 0 24 24");
 
-            const pathElement = document.createElementNS("http://www.w3.org/2000/svg", "path");
-            pathElement.setAttribute("d", "M7 7.00006L17 17.0001M7 17.0001L17 7.00006");
-            pathElement.setAttribute("stroke", "#b83d14");
-            pathElement.setAttribute("stroke-width", "2.5");
-            pathElement.setAttribute("stroke-linecap", "round");
-            pathElement.setAttribute("stroke-linejoin", "round");
+                const pathElement = document.createElementNS("http://www.w3.org/2000/svg", "path");
+                pathElement.setAttribute("d", "M7 7.00006L17 17.0001M7 17.0001L17 7.00006");
+                pathElement.setAttribute("stroke", "#b83d14");
+                pathElement.setAttribute("stroke-width", "2.5");
+                pathElement.setAttribute("stroke-linecap", "round");
+                pathElement.setAttribute("stroke-linejoin", "round");
 
-            svgElement.appendChild(pathElement);
-            removeButton.appendChild(svgElement);
+                svgElement.appendChild(pathElement);
+                removeButton.appendChild(svgElement);
 
-            removeButton.addEventListener("click", () => {
-                dispatchEvent("removeFolderButtonClicked", folder);
-            });
-            folderElement.append(folderTitle, removeButton);
+                removeButton.addEventListener("click", () => {
+                    dispatchEvent("removeFolderButtonClicked", folder);
+                });
+                removeButtonContainer.append(removeButton);
+            }
+            folderElement.append(folderTitle, removeButtonContainer);
             folderContainer.append(folderElement);
         }
         this.parent.append(folderContainer);
     }
     items(folder) {
+        console.log(folder.name);
         // creates dom items for the todo items within a folder
         const todoElements = createElement("div", { id: "todoElements" }, "");
         for (let todoElement of folder.items) {
@@ -56,6 +63,9 @@ class DisplayTools {
                 { class: "todoElementPriority" },
                 todoElement.getPriority()
             );
+
+            // remove button for each item
+            // X svg hardcoded because i was having problems importing
             const removeButton = createElement("div", { class: "removeButtonSvg" }, "");
             const svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
             svgElement.setAttribute("width", "40px");
--- src/modules/userInput/createFolder.js
+++ src/modules/userInput/createFolder.js
@@ -7,7 +7,11 @@ class CreateFolderDialogue {
     createFolderDialogue() {
         // creates the popup window that shows when 'new folder' clicked
         const container = createElement("div", { id: "createFolderContainer" }, "");
-        const textInput = createElement("input", { type: "text", id: "createFolderTextInput" }, "");
+        const textInput = createElement(
+            "input",
+            { type: "text", id: "createFolderTextInput", placeholder: "Folder Name" },
+            ""
+        );
         const submitButton = createElement("button", { id: "createFolderSubmitButton" }, "Submit");
         const closeButton = createElement("button", { id: "folderCloseButton" }, "Close");
         container.append(textInput, submitButton, closeButton);
